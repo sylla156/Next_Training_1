631d8b5ea2986aa3028c8fdf83d6c0e2944c0a85
sylla <s67986601@gmail.com>
2024-07-06 21:37:30 +0000

    Config(project): Configuration of the project

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -7,11 +7,14 @@ const config: Config = {
     "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
+    fontFamily: {
+      spartan: ["League Spartan", "sans-serif"],
+      hind: ["Hind", "sans-serif"],
+    },
     extend: {
-      backgroundImage: {
-        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
-        "gradient-conic":
-          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
+      colors: {
+        secondary: "#336699",
+        primary: "#FF6666",
       },
     },
   },
