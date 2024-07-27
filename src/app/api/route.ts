e8775f7bd8d3ad3305c2f941a07a22e716500e8c
sylla <s67986601@gmail.com>
2024-07-27 17:24:17 +0000

    hotfix

--- src/app/api/route.ts
+++ src/app/api/route.ts
@@ -1,15 +1,15 @@
 import { z } from "zod";
-import nodemailer from 'nodemailer';
+import nodemailer from "nodemailer";
 
-// const transporter = nodemailer.createTransport({
-//   host: process.env.BREVO_HOST,
-//   port: Number(process.env.BREVO_PORT),
-//   secure: false, // Use `true` for port 465, `false` for all other ports
-//   auth: {
-//     user: process.env.BREVO_USER,
-//     pass: process.env.BREVO_PASS,
-//   },
-// });
+const transporter = nodemailer.createTransport({
+  host: process.env.BREVO_HOST,
+  port: Number(process.env.BREVO_PORT),
+  secure: false, // Use `true` for port 465, `false` for all other ports
+  auth: {
+    user: process.env.BREVO_USER,
+    pass: process.env.BREVO_PASS,
+  },
+});
 
 export async function POST(request: Request) {
   const body = await request.json();
@@ -20,47 +20,45 @@ export async function POST(request: Request) {
     desc: z.string().optional(),
   });
 
-  console.log(body,{
-    host: process.env.BREVO_HOST,
-    port: Number(process.env.BREVO_PORT),
-    secure: false, // Use `true` for port 465, `false` for all other ports
-    auth: {
-      user: process.env.BREVO_USER,
-      pass: process.env.BREVO_PASS,
-    },
-  })
-   return Response.json({
+  if (formSchema.parse(body)) {
+    try {
+      const info = await transporter.sendMail({
+        from: `Portfolio User <${process.env.BREVO_SENDER}>`, // sender address
+        to: process.env.BREVO_RECEIVER, // list of receivers
+        subject: "Portfolio Email (Opportunites)", // Subject line
+        text: "Hello better", // mlain text body
+        html: `<div>
+    <h2> name: ${body.name} </h2> <br/>
+    <h2> email: ${body.name}</h2> <br/>
+    <h2> url: ${body.url} </h2> <br/>
+    <h2> desc:</h2> <p>${body.desc}</p><br/>
+      </div>`, // html body
+      });
+    } catch (error) {
+      return Response.json(
+        {
+          status: false,
+          content: "It seems like this shit don't work",
+        },
+        { status: 404 }
+      );
+    }
+
+    return Response.json({
       status: true,
       content: "Nice to meet you",
-    })
+    });
+  }
 
-  // if (formSchema.parse(body)) {
-  //   const info = await transporter.sendMail({
-  //     from: `Portfolio User <${process.env.BREVO_SENDER}>`, // sender address
-  //     to: process.env.BREVO_RECEIVER, // list of receivers
-  //     subject: "Portfolio Email (Opportunites)", // Subject line
-  //     text: "Hello better", // mlain text body
-  //     html: `<div>
-  //     <h2> name: ${body.name} </h2> <br/>
-  //     <h2> email: ${body.name}</h2> <br/>
-  //     <h2> url: ${body.url} </h2> <br/>
-  //     <h2> desc:</h2> <p>${body.desc}</p><br/>
-  //     </div>`, // html body
-  //   });
-
-  //   return Response.json({
-  //     status: true,
-  //     content: "Nice to meet you",
-  //   });
-  // }
-
-  // return Response.json(
-  //   {
-  //     status: false,
-  //     content: "It seems like this shit don't work",
-  //   },
-  //   { status: 404 }
-  // );
+  return Response.json(
+    {
+      status: false,
+      content: "It seems like this shit don't work",
+    },
+    { status: 404 }
+  );
 }
 
-
+export async function GET(request: Request) {
+  return Response.json({ body: `${process.env.RECEIVER}` });
+}
