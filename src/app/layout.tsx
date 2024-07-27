aad8aa6e4bb3a4cbccb3c07965b47f681c56df8b
sylla <s67986601@gmail.com>
2024-07-27 16:35:50 +0000

    Add(config): I Added the vercle traffic performance on my page

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -5,6 +5,7 @@ import ContentProvider from "@/context/ContentProvider";
 import ThemeProvider from "@/context/ThemeProvider";
 import React from "react";
 import { Analytics } from '@vercel/analytics/react';
+import { SpeedInsights } from '@vercel/speed-insights/next';
 
 export default function RootLayout({
   children,
@@ -20,6 +21,7 @@ export default function RootLayout({
             <Footer />
           </body>
           <Analytics />
+          <SpeedInsights />
         </html>
       </ContentProvider>
     </ThemeProvider>
